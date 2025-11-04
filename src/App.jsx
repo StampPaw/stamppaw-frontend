0cbbfc448fa9c5a45461582f54bbfa569ca06cbf
juyoung <juyoungjeong.797@gmail.com>
2025-11-04 13:11:54 +0900

    chore: setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
-import "./App.css";
+import HomePage from "./pages/HomePage";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
 
 export default App;
