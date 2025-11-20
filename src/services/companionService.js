d85871e3fa4efabf91e0013fda9c6c23e866a09d
gitseoyeon <tjdus553926@gmail.com>
2025-11-20 23:58:17 +0900

    hotfix: 동행글 페이지당 불러오는 데이터 증가

--- src/services/companionService.js
+++ src/services/companionService.js
@@ -1,7 +1,7 @@
 import api from "./api";
 
 // ✅ 동행 목록 조회
-export const getAllCompanions = async (page = 0, size = 10) => {
+export const getAllCompanions = async (page = 0, size = 20) => {
   const response = await api.get("/companion", {
     params: { page, size },
   });
